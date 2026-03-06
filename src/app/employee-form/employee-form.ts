711959d4b384f0ae57393eb64cc6a8b3e1ff2760
Golam Muktadir <rumi019aust@gmail.com>
2026-03-06 09:12:50 +0100

    employee form added

--- src/app/employee-form/employee-form.ts
+++ src/app/employee-form/employee-form.ts
@@ -1,11 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './employee-form.html',
   styleUrl: './employee-form.css',
 })
 export class EmployeeForm {
+  employeeForm: FormGroup;
+  showModal = false;
+  imagePreview: any;
+
+  constructor(private fb: FormBuilder) {
+
+    this.employeeForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phone: [''],
+      department: [''],
+      salary: [''],
+      address: ['']
+    });
+
+  }
+
+  previewData() {
+    this.showModal = true;
+  }
+
+  onImageUpload(event: any) {
+
+    const file = event.target.files[0];
+
+    if (file) {
+
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+
+      reader.readAsDataURL(file);
+
+    }
+
+  }
 
 }
